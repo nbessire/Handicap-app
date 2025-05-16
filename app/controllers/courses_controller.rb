357f51583da1dc81bf5e42f26af894e33d6da0bb
Emilia Rose <erose@bowdoin.edu>
2025-05-16 18:06:15 -0500

    Added course upload image and some styling

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -66,5 +66,6 @@ class CoursesController < ApplicationController
     # Only allow a list of trusted parameters through.
     def course_params
       params.expect(course: [ :name, :location, :par ])
+      params.require(:course).permit(:name, :location, :par, :image)
     end
 end
